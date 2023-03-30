d3d890b0e6b9af5d068d8bdadfda0b55f000202d
PrabhatAcharya <code2prabhat@gmail.com>
2023-03-30 10:43:46 +0530

    mongodbImplemented

--- server/index.js
+++ server/index.js
@@ -1,7 +1,10 @@
 // const express = require('express');
 import express from 'express';
+import Connection from './database/database.js';
 
 const app = express();
 const PORT=8000;
+
+Connection();
 
 app.listen(PORT,() => {console.log(`listening on port ${PORT}`)});
